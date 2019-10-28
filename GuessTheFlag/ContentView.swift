c86268444585afd22026f3226e7c74339b010f69
Levit Osei Kanner <lkanner21@gmail.com>
2019-10-28 18:00:46 +0000

    Initial commit

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,8 +9,84 @@
 import SwiftUI
 
 struct ContentView: View {
+    //Properties
+   @State private var countries = ["Estonia" , "Nigeria" , "France" , "UK" , "Germany" , "Ireland" , "Italy" , "Poland" , "Russia" , "Spain" , "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var alertMessage = ""
+    
+    
+    
+    // Body initialization
     var body: some View {
-        Text("Hello World")
+        NavigationView{
+            ZStack{
+                 LinearGradient(gradient: Gradient(colors: [.blue , .black]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                     .edgesIgnoringSafeArea(.all)
+                 VStack(spacing: 30){
+                     
+                     VStack{
+                         Text("Tap the flag of")
+                         .foregroundColor(.white)
+                         
+                         Text(countries[correctAnswer])
+                             .foregroundColor(.white)
+                             .font(.largeTitle)
+                             .fontWeight(.black)
+                          }
+                            
+                 ForEach(0..<3){number in
+                     Button(action: {
+                         //Flag tapped implementation
+                        self.flagTapped(number){
+                            self.alertMessage = $0
+                        }
+            
+                     }){
+                     Image(self.countries[number])
+                         .renderingMode(.original)
+                         }
+                     }
+                 .clipShape(Capsule())     //Puts the buttons in a capsule shape
+                 .overlay(Capsule().stroke(Color.black , lineWidth: 1))      //Add capsule styled border around flags
+                 .shadow(color: .black, radius: 2, x: 2, y: 2)
+                 .alert(isPresented: $showingScore) {
+                    Alert(title: Text(scoreTitle), message: Text("\(self.alertMessage)"), dismissButton: .default(Text("Continue")){
+                         self.askQuestion()
+                         })
+                     }
+                Text("Score: \(score)")
+                    .foregroundColor(.white)
+                    .font(.headline)
+                 Spacer()
+                    
+                 }
+             }
+        }
+ 
+    }
+    
+    
+    //Methods
+    func flagTapped(_ number: Int , message : (String)-> Void){
+        if number == correctAnswer {
+            scoreTitle = "That's Correct"
+            score += 1
+            message("You're a genius!")
+            
+        }else{
+            scoreTitle = "Wrong"
+            message("That's the flag of \(self.countries[number])")
+        }
+        showingScore = true
+    }
+    
+    
+    func askQuestion(){
+        countries = countries.shuffled()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
@@ -19,3 +95,21 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
+
+/*
+ What I've learnt today in SwiftUI
+1. Vstacks , Hstacks , ZStack , Spacers(To divide views)
+2. Colors and frames
+3. Secondary(Allows slight transparency for the color behind it to shine through it) and Primary colors in SwiftUI
+4. You can create custom colors : Color(red: , green: , blue: ) and pass numbers between 0 and 1.
+5. EdgesIgnoringSafeArea() to run over the safe area bounds.
+6. Gradients : LinearGradient , RadialGradient , AngularGradient; They can be used as stand-alone views or as background of views
+7. Buttons
+8.Images with decorative(Prevents screen-readers from reading Image titles) , systemName , nothing
+9. Adding renderingMode modifier to images to show their original images
+10.Alerts
+ */
